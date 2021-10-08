43dd8c283dbade6eb24d7cdc50b1cdfaa731d6f6
VoN <sudo.von.contact@gmail.com>
2021-10-07 23:17:26 -0500

    feat: :sparkles: Add ApolloClient

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+  uri: 'https://gapi.storyblok.com/v1/api',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        token: process.env.REACT_APP_STORYBLOCK_API_KEY,
+        version: 'draft'
+      }
+    })
+  }
+})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
